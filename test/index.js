74202ea927e051b1be1d8ad2cbfd2d3e8d85cd43
José Quintana <joseluisquintana20@gmail.com>
2018-07-11 22:22:33 +0200

    update testing

--- test/index.js
+++ test/index.js
@@ -1,29 +1,29 @@
-var fs = require('fs')
-var exec = require('child_process').exec
-var path = require('path')
-var test = require('blue-tape')
+const fs = require('fs')
+const { exec } = require('child_process')
+const path = require('path')
+const test = require('blue-tape')
 
-var OUT_FILENAME = path.join(__dirname, 'rules.out')
-var TSLINT_BIN = require.resolve('tslint/bin/tslint')
-var TSLINT_CMD = 'node ' + TSLINT_BIN + ' --config ../tslint.js --project tsconfig.json "rules/**/*.ts"'
+const OUT_FILENAME = path.join(__dirname, 'rules.out')
+const TSLINT_BIN = require.resolve('tslint/bin/tslint')
+const TSLINT_CMD = `node ${TSLINT_BIN} --config ../tslint.js --project tsconfig.json "rules/**/*.ts"`
 
-test('tslint standard', function (t) {
-  exec(TSLINT_CMD, { cwd: __dirname }, function (err, stdout, stderr) {
+test('tslint standard', (t) => {
+  exec(TSLINT_CMD, { cwd: __dirname }, (err, stdout, stderr) => {
     t.ok(err)
 
     const out = relatify(stdout, __dirname).trim()
 
     if (process.env.GENERATE_ASSETS) {
-      return fs.writeFile(OUT_FILENAME, out, function (err) {
+      return fs.writeFile(OUT_FILENAME, out, (err) => {
         t.notOk(err)
         t.end()
       })
     }
 
-    fs.readFile(OUT_FILENAME, 'utf8', function (err, result) {
+    fs.readFile(OUT_FILENAME, 'utf8', (err, result) => {
       t.notOk(err)
 
-      t.equal(out + '\n', result)
+      t.equal(out.trim(), result.trim())
       t.equal(stderr, '')
 
       t.end()
